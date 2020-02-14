0f77d8182dccded56273fccb9895b7ca56294b5e
Noah <dupasnoah@gmail.com>
2020-02-14 18:17:55 +0100

    roi

--- server_modules/Pieces/Roi.js
+++ server_modules/Pieces/Roi.js
@@ -1,5 +1,3 @@
-
-
 class Roi{
 
     constructor(x, y, player){
@@ -8,11 +6,36 @@ class Roi{
         this.player = player;
     }
 
-    isSafe(x,y){
-        /* detecter fou/reine */
-        /* detecter tour/reine */
-        /* detecter pion */
-        /* detecter cavalier */
+    verifier(pieceName,sens){
+        
+        for(let i = 0; i < sens.length; i++){
+            let x = this.x + 1*sens[i][0];
+            let y = this.y + 1*sensy[i][1];
+            if (isInBoard(x,y)) while(board[x][y].piece.couleur != this.couleur){
+                if (isInBoard(x,y)) for (let j = 0; j < pieceName.length; j++) if (board[x][y].piece.name == pieceName1) return 1;
+            }
+
+        }
+    }
+
+    verifier2(pieceName,sens){
+
+        for(let i = 1; i <= sens.length; i++){
+            let x = this.x + 1*sens[i][0];
+            let y = this.y + 1*sensy[i][1];
+            if (isInBoard(x,y)) if(board[x][y].piece.couleur != this.couleur){
+                for (let i = 0; i < pieceName.length; i++) if (board[x][y].piece.name == pieceName1) return 1;
+            }
+        }
+    }
+
+    isEnEcheque(){
+        if (verifier(["reine","fou"],[[1,1],[1,-1],[-1,1],[-1,-1]]) ||
+            verifier(["reine","tour"],[[1,0],[-1,0],[0,1],[0,-1]]) ||
+            verifier2(["pion"],[[1*Math.pow(-1,this.couleur + 1),1*Math.pow(-1,this.couleur + 1)],[1*Math.pow(-1,this.couleur + 1),-1*Math.pow(-1,this.couleur + 1)]]) ||
+            verifier2(["cavalier"],[[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]])) return 1;
+        
+        return 0;
     }
 
     playable(){
@@ -35,6 +58,6 @@ class Roi{
         
     }
 
-}
+};
 
 module.exports = Roi;
