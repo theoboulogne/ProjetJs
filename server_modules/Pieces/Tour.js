fbb2e75f9956e53e8418bc8b2202b6191bd8a4c4
Swann DR <swann.de_roquemaurel@isen.yncrea.fr>
2020-02-18 22:39:17 +0100

    playable R/T/F

--- server_modules/Pieces/Tour.js
+++ server_modules/Pieces/Tour.js
@@ -10,11 +10,17 @@ class Tour extends Piece{
         plateau.reset_playable()
         scan = [[-1,0][1,0][0,-1][0,1]];
         for (let j=0, i=1; j<scan.length; j++){
-            while(plateau.isInBoard((this.x + (scan[j][0] * i)),this.y + (scan[j][1] * i)) && (( this.x + (scan[j][0] * i),this.y + (scan[j][1] * i) ) != this.couleur) ){
-                if (( plateau.board[(this.x + (scan[j][0] * i))][this.y + (scan[j][1] * i)].piece != this.couleur) || (plateau.board[(this.x + (scan[j][0] * i))][this.y + (scan[j][1] * i)].piece == 0) ) {
-                    plateau.playable((this.x + (scan[j][0] * i)),this.y + (scan[j][1] * i),this.couleur)
+            while ( plateau.isInBoard( (this.x + (scan[j][0] * i)), (this.y + (scan[j][1] * i)) ) && (plateau.board[(this.x + (scan[j][0] * i))][this.y + (scan[j][1] * i)].piece != this.couleur) ){
+            // tant que que la positionXY est sur le plateau et qu'il n'y a pas de pion de la couleur du joueur à cette case :
+                if (plateau.board[(this.x + (scan[j][0] * i))][this.y + (scan[j][1] * i)].piece == 0) {     
+                    plateau.playable((this.x + (scan[j][0] * i)),this.y + (scan[j][1] * i),this.couleur);
+                    i++;        // si la case est vide, le scan continue
                 }
-                i++
+                else {
+                    plateau.playable((this.x + (scan[j][0] * i)),this.y + (scan[j][1] * i),this.couleur);
+                                // si il y a un pion adverse, le scan dans cette direction s'arrête (on sort du WHILE)
+                }
+                
             }
         }
     }
