de313d862e4296faafc0d1e9b7f16a82764d2cf8
Noah <dupasnoah@gmail.com>
2020-03-06 08:39:26 +0100

    roi

--- server_modules/Pieces/Roi.js
+++ server_modules/Pieces/Roi.js
@@ -4,13 +4,14 @@ class Roi{
         this.x = x;
         this.y = y;
         this.player = player;
+        this.hasMoved = 0;
     }
 
-    verifier(pieceName,sens){
+    verifier(pieceName,sens, x, y){
         
         for(let i = 0; i < sens.length; i++){
-            let x = this.x + 1*sens[i][0];
-            let y = this.y + 1*sensy[i][1];
+            x += 1*sens[i][0];
+            y += 1*sensy[i][1];
             if (isInBoard(x,y)) while(board[x][y].piece.couleur != this.couleur){
                 if (isInBoard(x,y)) for (let j = 0; j < pieceName.length; j++) if (board[x][y].piece.name == pieceName1) return 1;
             }
@@ -18,22 +19,22 @@ class Roi{
         }
     }
 
-    verifier2(pieceName,sens){
+    verifier2(pieceName,sens, x, y){
 
         for(let i = 1; i <= sens.length; i++){
-            let x = this.x + 1*sens[i][0];
-            let y = this.y + 1*sensy[i][1];
+            x += 1*sens[i][0];
+            y += 1*sensy[i][1];
             if (isInBoard(x,y)) if(board[x][y].piece.couleur != this.couleur){
                 for (let i = 0; i < pieceName.length; i++) if (board[x][y].piece.name == pieceName1) return 1;
             }
         }
     }
 
-    isEnEcheque(){
-        if (verifier(["reine","fou"],[[1,1],[1,-1],[-1,1],[-1,-1]]) ||
-            verifier(["reine","tour"],[[1,0],[-1,0],[0,1],[0,-1]]) ||
-            verifier2(["pion"],[[1*Math.pow(-1,this.couleur + 1),1*Math.pow(-1,this.couleur + 1)],[1*Math.pow(-1,this.couleur + 1),-1*Math.pow(-1,this.couleur + 1)]]) ||
-            verifier2(["cavalier"],[[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]])) return 1;
+    isEnEcheque(x,y){
+        if (verifier(["reine","fou"],[[1,1],[1,-1],[-1,1],[-1,-1]],x,y) ||
+            verifier(["reine","tour"],[[1,0],[-1,0],[0,1],[0,-1]],x,y) ||
+            verifier2(["pion"],[[1*Math.pow(-1,this.couleur + 1),1*Math.pow(-1,this.couleur + 1)],[1*Math.pow(-1,this.couleur + 1),-1*Math.pow(-1,this.couleur + 1)]],x,y) ||
+            verifier2(["cavalier"],[[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]],x,y)) return 1;
         
         return 0;
     }
@@ -48,14 +49,22 @@ class Roi{
         for (let i = 0; i < 3; i++){
             for (let j = 0; j < 3; j++){
                 if(isInBoard(X + i, Y + i)){
-                    
+                    if(!this.isEnEcheque(X + i, Y + i)){
+                        if(X + i != this.x & Y + i != this.y){
+                            isPlayable.push((X + i, Y + i));
+                        }
+                    }
                 }
             }
         }
+
+        return (isPlayable);
     }
 
     move(x,y){
-        
+        this.x = x;
+        this.y = y;
+        if (this.hasMoved == 0) this.hasMoved = 1;
     }
 
 };
