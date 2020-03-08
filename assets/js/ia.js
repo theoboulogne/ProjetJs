05604dc2d1f42b4fea526273f92e0c592bc21cef
Theo <theo.boulogne@isen.yncrea.fr>
2020-03-08 18:58:35 +0100

    ia

--- assets/js/ia.js
+++ assets/js/ia.js
@@ -1,17 +1,88 @@
-var valeurNosPieces = [
-    {id: '0', name: 'reine', valeur: '9'},
-    {id: '1', name: 'tour', valeur: '5'},
-    {id: '2', name: 'fou', valeur: '3'},
-    {id: '3', name: 'cavalier', valeur: '3'},
-    {id: '4', name: 'roi', valeur: '0'},
-    {id: '5', name: 'pion', valeur: '1'}
+var valeurPieces = [
+    {name: 'Reine', valeur: 9},
+    {name: 'Tour', valeur: 5},
+    {name: 'Fou', valeur: 3},
+    {name: 'Cavalier', valeur: 3},
+    {name: 'Roi', valeur: 0},
+    {name: 'Pion', valeur: 1}
 ]
+//pion
+function direction_libre(piece, board){
+    if(piece.constructor.name=='Pion'){//module par piece ?
+        for(let i=piece.y+Math.pow(-1,piece.couleur); i<8&&i>0; i+=Math.pow(-1,piece.couleur)){
+            if(piece.couleur==board[i].piece.couleur&&piece.constructor.name==board[i].piece.constructor.name){
+                return 2;
+            }
+        }
+    }
+    return 0;
+}
+function avancement(piece){
+    if(piece.constructor.name=='Pion'){//module par piece ?
+        return 0.5*(Math.abs(piece.y-(1+(piece.couleur*5))));
+    }
+}
 
-var valeurNosPieces = [
-    {id: '0', name: 'reine', valeur: '9'},
-    {id: '1', name: 'tour', valeur: '5'},
-    {id: '2', name: 'fou', valeur: '3'},
-    {id: '3', name: 'cavalier', valeur: '3'},
-    {id: '4', name: 'roi', valeur: '0'},
-    {id: '5', name: 'pion', valeur: '1'}
-]
+
+//tour
+function Tour()
+
+
+
+
+
+//Autre -> a mettre dans la classe ia directement
+function Prise(piecePrise){ 
+    if(0!=piecePrise){
+        let valeur = 0
+        //prise de la piece
+        for(let i=0; i<valeurPieces; i++){if(valeurPieces[i].name == piecePrise.constructor.name) valeur += valeurPieces[i].valeur;};
+        return valeur;
+}
+
+function Menace(piece, piecePrise){
+    if(0!=piecePrise){
+        let valeur = 0
+        //                           methode de echec a récupérer et mettre la couleur en argument + récupérer la liste ennemi menaçant et allie qui protegent
+
+        //si notre piece peut être prise
+        if(true){ // si echec a mettre
+            for(let i=0; i<valeurPieces; i++){if(valeurPieces[i].name == piece.constructor.name) valeur -= valeurPieces[i].valeur;};
+
+            //Privilégier de continuer tant que nballie>nbennemi, ajouter dans le tableau à renvoyer directement ?
+
+/*                 A coder :
+
+            classer ennemi en fonction de la valeur des pieces
+                tant que Nbennemi > Nballie 
+                    retirer Nbennemi le plus fort
+            pareil en inversant la condition et en retirant Nballie le plus fort , methode a faire ?
+            
+
+
+            listeennemival = listeennemi*valeur de chaque(boucle a faire) 
+            pareil pour listeallieval
+
+            valeur += listeallieval-listeennemival
+
+*/
+            
+    }
+}
+
+function valeur_ia(piece, plateau, piecePrise){ // val par defaut de pieceprise  a mettre
+    let valeur = 0
+    
+    for(let i=0; i<valeurPieces; i++){if(valeurPieces[i].name == piece.constructor.name) valeur += valeurPieces[i].valeur;};
+
+    valeur += direction_libre(piece, plateau.board)
+    valeur += avancement(piece)
+
+    valeur += Prise(piecePrise)
+    valeur += Menace(piece, piecePrise) // potentiellement retirer piecePrise en déplacant la condition en amount
+
+    //si peut prendre le roi methode a faire
+    //valeur max +1 ?
+
+
+}
