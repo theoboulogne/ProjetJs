19c69ca1e837549efb14795682e895b5d595176b
Noah <dupasnoah@gmail.com>
2020-03-08 17:01:52 +0100

    ia

---
+++ assets/js/ia.js
@@ -0,0 +1,17 @@
+var valeurNosPieces = [
+    {id: '0', name: 'reine', valeur: '9'},
+    {id: '1', name: 'tour', valeur: '5'},
+    {id: '2', name: 'fou', valeur: '3'},
+    {id: '3', name: 'cavalier', valeur: '3'},
+    {id: '4', name: 'roi', valeur: '0'},
+    {id: '5', name: 'pion', valeur: '1'}
+]
+
+var valeurNosPieces = [
+    {id: '0', name: 'reine', valeur: '9'},
+    {id: '1', name: 'tour', valeur: '5'},
+    {id: '2', name: 'fou', valeur: '3'},
+    {id: '3', name: 'cavalier', valeur: '3'},
+    {id: '4', name: 'roi', valeur: '0'},
+    {id: '5', name: 'pion', valeur: '1'}
+]
